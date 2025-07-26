fd6f8a23aa594418ccd81214529c0029fd3d2b6b
AmirSidka <amirmohammadyosefi290@gmail.com>
2025-07-26 14:49:20 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
-// config.js
 module.exports = {
-  botToken: "7616960362:AAE3Xz0cbYXjmWnE0QlqGBAA79I0hHQpsvY",
-  adminId: "7549513123", // مثلا: "1371365451"
-  sponsorChannel: "SidkaShop", // بدون @
-  channelWithAt: "@SidkaShop", // با @
-  botUsername: "AmirSidkaV2rayNG_Bot", // بدون @
+  botToken: "7616960362:AAE3Xz0cbYXjmWnE0QlqGBAA79I0hHQpsvY",  // حتما جایگزین کن
+  adminId: "7549513123",
+  sponsorChannel: "SidkaShop",  // بدون @
+  channelWithAt: "@SidkaShop",  // با @
+  botUsername: "AmirSidkaV2rayNG_Bot",  // بدون @
 };
